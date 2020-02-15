8926dc87ddf4d30787fddddb43a0a13e4f5f8c6c
Andre Dias <and.ddias@gmail.com>
2020-02-15 10:40:41 -0300

    Testando se Usuario está logado e usando Djando Shell

--- core/views.py
+++ core/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 
 
 def index(request):
+    # Usando informações do Django Shell
+    print(dir(request.user))
+    print(f"User: {request.user}")
+
+    # Logica para saber se usuario está logado
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuário não logado'
+    else:
+        teste = 'Usuário logado'
+
     context = {
         'curso': 'Programação Web com Django Framework',
         'outro': 'Django é massa!',
+        'logado': teste,
     }
     return render(request, 'index.html', context)
 
