d353be03106797796dd7f2e5fbe010510e1545be
Andre Dias <and.ddias@gmail.com>
2020-02-15 15:18:44 -0300

    Tratando erros 500(erro de processamento) e 404(pagina não encontrada)

--- django1/urls.py
+++ django1/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+handler404 = views.error404
+handler500 = views.error500
